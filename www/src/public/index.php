0dffe5ac08aa2fc0a7129d1b56a701c327ff9af2
nurbxfit <nurbxfit@gmail.com>
2024-07-20 23:58:34 +0800

    added some php code example

--- www/src/public/index.php
+++ www/src/public/index.php
@@ -1,4 +1,24 @@
 <?php
 
-echo "Peace to the world!";
+require '../App/ErrorController.php';
+$error_controller = new ErrorController;
+
+$method = strtolower($_SERVER['REQUEST_METHOD']);
+$path = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+$path_parts = explode("/",parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH));
+
+if($path == '/' && $method == 'get'){
+    require '../App/HomeController.php';
+    $home_controller = new HomeController;
+    $home_controller->get();
+}else if ($path == '/movies' && $method == 'get' ){
+
+    require '../App/MovieController.php';
+    $movie_controller = new MovieController;
+    $movie_controller->get();
+}else {
+    $error_controller->notFound();
+    exit();
+}
+
 
